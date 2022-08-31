9b65aeef9ff7e47d8639e348a98bb2add67cf4c2
pythonpete32 <aaron@blackscale.io>
2022-08-31 20:39:18 +0400

    🧪 testWithdrawLpWithoutInsurance

--- packages/contracts/test/Integration.t.sol
+++ packages/contracts/test/Integration.t.sol
@@ -11,7 +11,7 @@ contract IntegrationTest is BaseTest {
         assertEq(usd.balanceOf(lp2), HUNDRED_K);
     }
 
-    function testDepositLP() public {
+    function testDepositLpWithoutInsurance() public {
         vm.startPrank(lp1);
         usd.approve(address(pool), HUNDRED_K);
         pool.deposit(HUNDRED_K, lp1);
@@ -23,5 +23,28 @@ contract IntegrationTest is BaseTest {
         assertEq(usd.balanceOf(address(pool)), HUNDRED_K * 2);
         assertEq(pool.balanceOf(lp1), HUNDRED_K);
         assertEq(pool.balanceOf(lp2), HUNDRED_K);
+        vm.stopPrank();
+    }
+
+    function testWithdrawLpWithoutInsurance() public {
+        vm.startPrank(lp1);
+        usd.approve(address(pool), HUNDRED_K);
+        pool.deposit(HUNDRED_K, lp1);
+
+        changePrank(lp2);
+        usd.approve(address(pool), HUNDRED_K);
+        pool.deposit(HUNDRED_K, lp2);
+
+        assertEq(usd.balanceOf(address(pool)), HUNDRED_K * 2);
+        assertEq(pool.balanceOf(lp1), HUNDRED_K);
+        assertEq(pool.balanceOf(lp2), HUNDRED_K);
+
+        // withdraw
+        changePrank(lp1);
+        pool.withdraw(HUNDRED_K, lp1, lp1);
+        assertEq(usd.balanceOf(address(lp1)), HUNDRED_K);
+        assertEq(pool.balanceOf(lp1), 0);
+        assertEq(usd.balanceOf(address(pool)), HUNDRED_K);
+        vm.stopPrank();
     }
 }
